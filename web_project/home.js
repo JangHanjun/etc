21a78b74ab2cabb9f774078ce6842f4cfebd7a26
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-10 22:13:06 +0900

    일단 만들어본 팝업

---
+++ web_project/home.js
@@ -0,0 +1,18 @@
+
+
+function reImg(){
+  document.getElementById('mainimg').src = "room.png"
+}
+
+function movie(){
+  document.getElementById('mainimg').src = "room_movie.png"
+}
+function trip(){
+  document.getElementById('mainimg').src = "room_Trip.png"
+}
+function music(){
+  document.getElementById('mainimg').src = "room_Cafe.png"
+}
+function sleep(){
+  document.getElementById('mainimg').src = "room_Bed.png"
+}
